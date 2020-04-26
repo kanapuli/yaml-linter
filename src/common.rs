8540404ad070f08c81377c752efa0d6d8fd11092
kanapuli <athavankanapuli@gmail.com>
2020-04-26 17:41:08 +0530

    Implement From trait for File struct
    Implement from trait to convert PathBuf to File

--- src/common.rs
+++ src/common.rs
@@ -3,3 +3,20 @@ pub struct File {
     pub file_name: String,
     pub path: PathBuf,
 }
+
+impl File {
+   fn from(path: PathBuf) -> Option<Self>{
+       let file_name = match path.file_name(){
+           Some(f) => f,
+           None => return None,
+       };
+       
+       let file_name = match file_name.to_str(){
+           Some(f) => f.to_string(),
+           None => return None
+       };
+
+       Some(File{file_name, path})
+
+   } 
+}
