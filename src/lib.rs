b81493a7dc95b510dcb0e76b90a254af3337b76f
kanapuli <athavankanapuli@gmail.com>
2020-04-26 17:19:02 +0530

    Canonicalize and group all input directories

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
 extern crate clap;
 
-use clap::{App,Arg};
+use clap::{App,Arg,ArgMatches};
 use std::env;
 use std::error::Error;
+use std::fs;
 use std::ffi::OsStr;
+use std::path::PathBuf;
 
 //Warning is the lint warnings
 pub struct Warning{}
 
 //get_args gets the arguments from users
-fn get_args(current_dir: &OsStr) {
+fn get_args(current_dir: &OsStr) -> clap::ArgMatches {
     let matches = App::new(env!("CARGO_PKG_NAME"))
                 .version(env!("CARGO_PKG_VERSION"))
                 .author(env!("CARGO_PKG_AUTHORS"))
@@ -18,15 +20,19 @@ fn get_args(current_dir: &OsStr) {
                     Arg::with_name("input")
                     .short("i")
                     .long("input")
-                    .takes_value(true)
                     .help("file names or directory name to check")
+                    .index(1)
+                    .takes_value(true)
                     .default_value_os(current_dir)
+                    .multiple(true)
+                    .required(true)
                 ).get_matches();
-
+    return matches;
 }
 
 
 fn run() -> Result<Vec<Warning>, Box<dyn Error>> {
+    let warnings= Vec::new();
    //set default directory
    let current_directory = match env::current_dir() {
         Ok(dir) => dir,
@@ -35,4 +41,14 @@ fn run() -> Result<Vec<Warning>, Box<dyn Error>> {
    let current_directory = current_directory.as_os_str();
    
    let args =  get_args(current_directory);
+   let mut dirs: Vec<PathBuf> = Vec::new();
+
+   if let Some(inputs) = args.values_of("input") {
+      dirs = inputs
+      .clone()
+      .filter_map(|s| fs::canonicalize(s).ok()) 
+      .filter(|p| p.is_dir())
+      .collect();
+   };
+   Ok(warnings)
 }
